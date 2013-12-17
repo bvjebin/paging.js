983e439ef25355ac06022d903a0b7023b929e0a2
Jebin <jebin@datarpm.com>
2013-12-17 22:59:00 +0530

    stealth mode demo

---
+++ js/compressor.js
@@ -0,0 +1,48 @@
+(function() {
+
+	var _fs = require('fs');
+	var UglifyJS = require('uglify-js');
+	var path = "js/", pagingjs, code, minifiedCurrJs, getUglifiedCode;
+
+	//read file
+	pagingjs = _fs.readFileSync(path+"paging.js", 'utf8');
+
+	getUglifiedCode = function(code) {
+		var ast, compressor;
+		ast = UglifyJS.parse(code);
+		ast.figure_out_scope();
+		compressor = UglifyJS.Compressor({
+			sequences     : true,  // join consecutive statemets with the “comma operator”
+			properties    : true,  // optimize property access: a["foo"] → a.foo
+			dead_code     : true,  // discard unreachable code
+			drop_debugger : true,  // discard “debugger” statements
+			unsafe        : false, // some unsafe optimizations
+			conditionals  : true,  // optimize if-s and conditional expressions
+			comparisons   : true,  // optimize comparisons
+			evaluate      : true,  // evaluate constant expressions
+			booleans      : true,  // optimize boolean expressions
+			loops         : true,  // optimize loops
+			unused        : true,  // drop unused variables/functions
+			hoist_funs    : true,  // hoist function declarations
+			hoist_vars    : false, // hoist variable declarations
+			if_return     : true,  // optimize if-s followed by return/continue
+			join_vars     : true,  // join var declarations
+			cascade       : true,  // try to cascade `right` into `left` in sequences
+			side_effects  : true,  // drop side-effect-free statements
+			warnings      : true,  // warn about potentially dangerous optimizations/code
+			global_defs   : {}     // global definitions
+		});
+		ast = ast.transform(compressor);
+		return ast.print_to_string();
+	};
+
+	//uglify
+	code = getUglifiedCode(pagingjs);
+
+    //creating new file name with time stamp
+    minifiedCurrJs = path + "/paging.min.js";
+
+	//writing file to min directory in drpmjs directory
+	_fs.writeFileSync(minifiedCurrJs, code, {encoding:'utf8'});
+
+})();
